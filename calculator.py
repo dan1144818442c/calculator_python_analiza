c421fb44f8a1025a0268a164eeeade57ab9657a1
d.c! <daniel.c99999@gmail.com>
2025-06-24 17:22:51 +0300

    commit class Rectangle

--- calculator.py
+++ calculator.py
@@ -1,1 +1,6 @@
-class
+class Shape:
+    def __init__(self , high , side):
+        self.high  = high
+        self.side = side
+    def get_area(self):
+        return  self.side*self.high
---
+++ class_Rectangle.py
@@ -0,0 +1,7 @@
+import calculator
+
+
+class Rectangle(calculator.Shape):
+    def __init__(self , high , side):
+        super().__init__( high , side)
+
